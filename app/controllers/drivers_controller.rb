c5096aca05b503708e285a5f87991fea0750fbf7
Liel van der Hoeven <lielvan1234@gmail.com>
2019-08-24 21:49:56 +0300

    Refactor drivers pages

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,6 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all
+    @drivers = Driver.all.to_json
     @cars = Car.all
   end
 
---
+++ app/javascript/Drivers.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Date Of Birth</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="(driver, index) in drivers" :key="index">
+          <td scope="row"> {{ driver.name }} </td>
+          <td> {{ driver.email }} </td>
+          <td> {{ driver.date_of_birth }} </td>
+          <td><a class="btn btn-info" :href="'drivers/' + driver.id">Show</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'drivers' ],
+}
+</script>
+
+<style>
+  
+</style>
