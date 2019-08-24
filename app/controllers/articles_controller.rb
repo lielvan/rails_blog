79ed4cb71f6726d431910218d20a9d298a33eae4
Liel van der Hoeven <lielvan1234@gmail.com>
2019-08-24 21:49:11 +0300

    Refactor articles pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all.to_json
   end
 
   def show
---
+++ app/javascript/Articles.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Text</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="(article, index) in articles" :key="index">
+          <td scope="row"> {{ article.title }} </td>
+          <td> {{ article.text }} </td>
+          <td><a class="btn btn-info" :href="'articles/' + article.id">Show</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'articles' ],
+}
+</script>
+
+<style>
+  
+</style>
