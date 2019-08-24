f0b19346f6498da42c2ef409ca0198c4996ee1c6
Liel van der Hoeven <lielvan1234@gmail.com>
2019-08-24 21:48:28 +0300

    Refactor welcome page

---
+++ app/javascript/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <a href="/articles">Blog</a> |
+    <a href="/drivers">Drivers</a> |
+    <a href="/cars">Cars</a> |
+    <a href="/driversandcars">Drivers & Cars</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Cnvrg.io Rails Assignment"
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  font-size: 2em;
+  text-align: center;
+}
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
